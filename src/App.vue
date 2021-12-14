57ede0580e68d24992071a321e6a7d4689d34b14
monodyle <monodylh@gmail.com>
2021-12-14 23:36:48 +0700

    remove chart

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
   <div class="flex items-center justify-center w-full h-screen text-center">
     <div class="w-full max-w-xs p-6">
       <div
-        class="flex items-center justify-center text-5xl font-semibold text-gray-400"
+        class="flex items-center justify-center text-6xl font-bold text-gray-400 "
       >
-        {{ coin.toUpperCase() }}
+        {{ symbol.toUpperCase() }}
       </div>
       <div v-if="last">
         <span
@@ -21,82 +21,67 @@
         </span>
       </div>
     </div>
-    <div class="absolute bottom-0 left-0 w-full overflow-hidden">
-      <div class="-ml-6 -mr-2 pointer-events-none">
-        <chart :prices="data" />
-      </div>
-    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { reactive, ref } from "@vue/reactivity";
+import { ref } from "@vue/reactivity";
 import { onMounted, onUnmounted } from "@vue/runtime-core";
 import { currencyFormatter, socketChange } from "~/utils";
-import chart from "~/components/chart.vue";
 import { SocketResponse } from "~/interfaces/PriceData";
 import { DELAY_TIME } from "~/constants";
 
 export default {
-  components: { chart },
   setup() {
-    const data = reactive<number[][]>([]);
     const prev = ref<string>("");
     const last = ref<string>("");
-    const coin = ref<string>("btc");
+    const symbol = ref<string>("btc");
     let mess: SocketResponse = {};
-    let readyFlag: boolean = true;
 
     const socket: WebSocket = new WebSocket(`wss://stream.binance.com/stream`);
     socket.onopen = function () {
-      socketChange(socket, "SUBSCRIBE", coin.value);
+      socketChange(socket, "SUBSCRIBE", symbol.value);
     };
     socket.onmessage = function (message) {
       const parser: SocketResponse = JSON.parse(message.data);
       const response = parser.data;
       if (!response) return;
-      if (readyFlag) {
-        data.push([response.E, parseFloat(response.p)]);
-        readyFlag = false;
-      }
       mess = parser;
     };
 
     onMounted(() => {
       const urlHash: string = window.location.hash?.replace("#", "");
       if (urlHash) {
-        localStorage.coin = urlHash;
-        coin.value = urlHash;
-      } else if (localStorage.coin) {
-        coin.value = localStorage.coin;
+        localStorage.symbol = urlHash;
+        symbol.value = urlHash;
+      } else if (localStorage.symbol) {
+        symbol.value = localStorage.symbol;
       } else {
-        localStorage.coin = coin.value;
+        localStorage.symbol = symbol.value;
       }
 
       window.onhashchange = () => {
         const hash: string = window.location.hash?.replace("#", "");
         if (hash !== "") {
-          socketChange(socket, "UNSUBSCRIBE", coin.value);
-          localStorage.coin = hash;
-          coin.value = localStorage.coin;
-          socketChange(socket, "SUBSCRIBE", coin.value);
+          socketChange(socket, "UNSUBSCRIBE", symbol.value);
+          localStorage.symbol = hash;
+          symbol.value = localStorage.symbol;
+          socketChange(socket, "SUBSCRIBE", symbol.value);
         }
       };
     });
 
     onUnmounted(() => {
-      socketChange(socket, "UNSUBSCRIBE", coin.value);
+      socketChange(socket, "UNSUBSCRIBE", symbol.value);
     });
 
     setInterval(function () {
       prev.value = last.value;
       last.value = mess.data?.p || "";
-      readyFlag = true;
     }, DELAY_TIME);
 
     return {
-      coin,
-      data,
+      symbol,
       prev,
       last,
       currencyFormatter,
